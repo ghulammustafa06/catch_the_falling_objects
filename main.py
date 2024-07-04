768abd99bcebe86062e57367d140c1f0b49a8a4e
Ghulam Mustafa <130176095+ghulammustafa06@users.noreply.github.com>
2024-07-04 13:45:16 -0700

    Designed player character.

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
+
+
+player = pygame.Rect(375, 500, 50, 50)  # Player as a rectangle
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (0, 255, 0), player)
+    pygame.display.flip()
